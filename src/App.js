e4a759f2a3c1dd2bd14c3675e6e5d985e482db85
valentinbr16 <bruniauv@gmail.com>
2021-05-14 12:59:26 +0200

    Changement de dossier

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './App.css';
+import Card from './Card';
+import Example from './Example';
+
+function App() {
+  return (
+    <div className="App">
+      <Example />
+    </div>
+  );
+}
+
+export default App;
