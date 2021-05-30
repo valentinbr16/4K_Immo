3d0bd03dd5255cbc2a30f0a14eb42318ae692b4e
valentinbr16 <bruniauv@gmail.com>
2021-05-30 20:58:12 +0200

    Amélioration formulaire input motif

--- src/Form.js
+++ src/Form.js
@@ -6,13 +6,29 @@ export default function Form() {
     return (
         <form className="form">
             <div id="contact" className="form__name">
-                <input className="form__firstname" id="firstname" placeholder="Nom" type="text" name="firstname" id="firstname" />
-    
-                <input className="form__lastname" id="lastname" placeholder="Prénom" type="text" name="lastname" id="lastname" />
+                <div>
+                    <input className="form__firstname" id="firstname" placeholder="Nom" type="text" name="firstname" id="firstname" />
+                </div>
+                <div>
+                    <input className="form__lastname" id="lastname" placeholder="Prénom" type="text" name="lastname" id="lastname" />
+                </div>
+
             </div>
 
             <div>
                 <input className="form__mail" id="mail" placeholder="Adresse mail" type="email" name="email" id="email" />
+            </div>
+
+            <div>
+                <div>
+                    <label className="form__label" htmlFor="reason">Veuillez choisir un motif :</label>
+                </div>
+                <select className="form__reason" name="reason" id="reason" placeholder="Veuillez choisir un motif" required>
+                    <option value="renseignement">Renseignement</option>
+                    <option value="visite">Demande de visite</option>
+                    <option value="prereservation">Préréservation</option>                    
+                    <option value="autre">Autres</option>
+                </select>
             </div>
 
             <div>
