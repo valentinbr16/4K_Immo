c2d8303853129bd6781b89d8d54716f6c5a24573
valentinbr16 <bruniauv@gmail.com>
2021-05-30 20:08:58 +0200

    Amélioration CSS header & navbar

--- src/Header.js
+++ src/Header.js
@@ -5,8 +5,11 @@ export default function Header() {
     return (
             <div className="header__banner">
                 <img className="header__logo" src="./logo4K_Immo.png" alt="Logo 4K Immo" />
-                <a className="header__link" href="#cards">Logements</a>
-                <a className="header__link" href="#contact">Contact</a>
+                <div className="header__navbar">
+                    <a className="header__link" href="#presentation">Présentation</a>
+                    <a className="header__link" href="#cards">Logements</a>
+                    <a className="header__link" href="#contact">Contact</a>
+                </div>
             </div>        
     )
 }
