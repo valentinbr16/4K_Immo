cc89526a92b0b886bcfd2cd5d010190f677bbc65
valentinbr16 <bruniauv@gmail.com>
2021-06-04 21:23:36 +0200

    Amélioration sémantique remplacement div

--- src/Header.js
+++ src/Header.js
@@ -3,13 +3,13 @@ import './Header.css';
 
 export default function Header() {
     return (
-            <div className="header__banner">
+            <header className="header__banner">
                 <img className="header__logo" src="./logo4K_Immo.png" alt="Logo 4K Immo" />
                 {/* <div className="header__navbar">
                     <a className="header__link" href="#presentation">Présentation</a>
                     <a className="header__link" href="#cards">Logements</a>
                     <a className="header__link" href="#contact">Contact</a>
                 </div> */}
-            </div>        
+            </header>        
     )
 }
