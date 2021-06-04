6e644a7a534200bcda7835959c254a1e457987e9
valentinbr16 <bruniauv@gmail.com>
2021-06-04 21:18:27 +0200

    Ajout component Intro et intégration dans App.js
    Permet d'introduire une carte
    ainsi qu'une description des logements et de l'agence

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {useState} from 'react';
 import './App.css';
 import Card from './Card';
 import Header from './Header';
+import Intro from './Intro';
 import Form from './Form';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Intro />
       <div className="card--flex">
         <Card type="STUDIO" surface="33m²" city="Saint-Omer" />
         <Card type="T3" surface="80m²" city="Lens" />
---
+++ src/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Intro.css';
+
+export default function Intro() {
+    return (
+        <section className="intro">
+            <img className="intro__img" src="Carte_4KImmo.png" alt="Carte de france avec location des logements indiqués"/>
+        </section>
+    )
+}
